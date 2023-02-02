f25eeb4c8f44bac79d220d8554eaec927128aca3
= <=>
2023-02-02 20:31:07 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+// css files
+import '../sass/app.scss'
+import '../css/style.css'
+
+
+
+
+
+
+// javascript files
+import swal from 'sweetalert2/dist/sweetalert2.js';
+import jQuery from 'jquery';
+import DataTable from 'datatables.net';
+
+//global variables
+window.Swal = swal;
+window.$ = jQuery;
+window.DataTable = DataTable;
+
+import * as bootstrap from 'bootstrap'
+import * as datatable from 'datatables.net-bs5'
+
+
+
